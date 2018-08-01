180a079235557d3c69aaeeedc1a7b3a89b63cbab
Chao <nickzhang0608@gmail.com>
2018-07-31 17:19:58 -0700

    Email generation for missing product in Atlas

--- routes/missingProduct.js
+++ routes/missingProduct.js
@@ -10,37 +10,42 @@ var config = JSON.parse(fs.readFileSync("config.json"));
 
 sgMail.setApiKey(config.API_KEY);
 
-/* GET users listing. */
 router.get('/', function (req, res, next) {
-
     var startDate = req.query.from;
     var endDate = req.query.to;
 
-    var myUrl = new URL("https://blue-sellapi.tugo.com/monitor/api/list/report?fromTime=&toTime=");
+    var myUrl = new URL("https://blue-sellapi.tugo.com/monitor/api/list/report?monitorType=P2V-POLICY-TRANSFER-STATUS&fromTime=&toTime=");
 
-    console.log(myUrl.href);
     myUrl.searchParams.set('fromTime', startDate);
     myUrl.searchParams.set('toTime', endDate);
     console.log(myUrl.href);
-
-
-//todo make the data range dynamic
 
     const external = {
         method: 'GET',
         url: myUrl,
         json: true
     };
-    console.log(external.url);
+    // console.log(external.url);
+    var result = [];
 
     rp(external)
         .then(function (response) {
+            // console.log(response.content);
+            for (var i = 0; i < response.content.length; i++) {
+                if (response.content[i].error.toString().includes("PRODUCT_NOT_FOUND") || response.content[i].error.toString().includes("Product:")) {
+                    result.push(response.content[i]);
+                    console.log("Find it");
+                }
+            }
+            // console.log(result);
+            var finalResult = {"content" : result};
+            // console.log(finalResult);
 
             var data = {
                 template: {
-                    'shortid': 'BJqsE80E7'
+                    'shortid': 'ry8KRP0NQ'
                 },
-                data: response
+                data: finalResult
             };
 
             var options = {
@@ -50,9 +55,9 @@ router.get('/', function (req, res, next) {
             };
 
             request(options)
-                .pipe(fs.createWriteStream('policy failure.xlsx')).on('finish', function () {
+                .pipe(fs.createWriteStream('missing product setup in Atlas.xlsx')).on('finish', function () {
                 sendEmail(startDate, endDate);
-                res.render('policyFailure', { title: 'Policy failure report from ' + startDate + ' to ' + endDate + " send out" });
+                res.render('missingProduct', { title: 'Missing product setup in Atlas from ' + startDate + ' to ' + endDate + " send out" });
             })
                 .on('error', function (err) {
                     console.log(err.message);
@@ -64,22 +69,23 @@ router.get('/', function (req, res, next) {
         })
         .catch(function (error) {
             console.log(error)
-        })
+        });
 
 });
 
-  function sendEmail(start, end) {
-    var data = fs.readFileSync('./policy failure.xlsx');
+
+function sendEmail(start, end) {
+    var data = fs.readFileSync('./missing product setup in Atlas.xlsx');
 
     const msg = {
         to: 'czha@tugo.com',
         from: 'test@tugo.com',
-        subject: 'Policy transfer failure report ' + 'from ' + start + ' to ' + end,
-        text: 'Policy transfer failure',
+        subject: 'Missing product setup in Atlas ' + 'from ' + start + ' to ' + end,
+        text: 'Missing product setup in Atlas',
         attachments: [
             {
                 content: new Buffer(data).toString('base64'),
-                filename: 'Policy transfer failure ' + start + ' to ' + end + '.xlsx',
+                filename: 'Missing product setup in Atlas from ' + start + ' to ' + end + '.xlsx',
                 type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                 disposition: 'attachment'
             }
@@ -94,6 +100,5 @@ router.get('/', function (req, res, next) {
     });
 
 }
-
 
 module.exports = router;
