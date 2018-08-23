67f4230a5f223c53a338ef9de1cf0682ff2b576e
Chao <nickzhang0608@gmail.com>
2018-08-23 16:53:03 -0700

    add alternative solution to missing product

--- routes/missingProduct.js
+++ routes/missingProduct.js
@@ -5,9 +5,12 @@ var rp = require('request-promise');
 const sgMail = require('@sendgrid/mail');
 var fs = require('fs');
 var URL = require('url').URL;
+var json2xls = require('json2xls');
 
 var config = JSON.parse(fs.readFileSync("config.json"));
 var POLICY_FAILURE_URL = JSON.parse(fs.readFileSync("URLS.json"));
+var JS_REPORT_TEST = POLICY_FAILURE_URL.JS_REPORT_TEST;
+var JS_REPORT = POLICY_FAILURE_URL.JS_REPORT;
 
 sgMail.setApiKey(config.API_KEY);
 
@@ -17,7 +20,7 @@ router.get('/', function (req, res) {
 
 });
 
-router.post('/',function (req, res) {
+router.post('/', function (req, res) {
     console.log(req.body.Email);
     console.log(req.body.from);
     console.log(req.body.to);
@@ -39,57 +42,100 @@ router.post('/',function (req, res) {
     };
     var result = [];
 
-    rp(external)
-        .then(function (response) {
-            // console.log(response.content);
-            for (var i = 0; i < response.content.length; i++) {
-                if (response.content[i].error.toString().includes("PRODUCT_NOT_FOUND") || response.content[i].error.toString().includes("Product:")) {
-                    result.push(response.content[i]);
-                    console.log("Find it");
-                }
+    var http = require('http'),
+        url = require('url'),
+        options = {
+            method: 'HEAD',
+            host: url.parse(JS_REPORT_TEST).host,
+            port: 80,
+            path: url.parse(JS_REPORT_TEST).pathname
+        },
+        testAPI = http.request(options, function (r) {
+            jsReport = JSON.stringify(r.statusCode);
+            console.log(jsReport);
+            if (jsReport === '200') {
+                console.log('js report server is up, using js report to generate excel');
+                rp(external)
+                    .then(function (response) {
+                        // console.log(response.content);
+                        for (var i = 0; i < response.content.length; i++) {
+                            if (response.content[i].error.toString().includes("PRODUCT_NOT_FOUND") || response.content[i].error.toString().includes("Product:")) {
+                                result.push(response.content[i]);
+                                // console.log("Find it");
+                            }
+                        }
+                        var finalResult = {"content": result};
+
+                        var data = {
+                            template: {
+                                'shortid': 'BJ8POGsL7'
+                            },
+                            data: finalResult
+                        };
+
+                        var options = {
+                            url: JS_REPORT,
+                            method: 'POST',
+                            json: data
+                        };
+
+                        request(options).on('error', function (error) {
+                            res.render('error', {error: error});
+                        })
+                            .pipe(fs.createWriteStream('missing product setup in Atlas.xlsx')).on('finish', function () {
+                            sendEmail(startDate, endDate, email);
+                            res.render('finishPage', {status: ""});
+                        })
+                            .on('error', function (err) {
+                                res.render('error', {error: err});
+                                console.log(err.message);
+                            });
+
+
+                        console.log("finish write in file!!");
+
+                    })
+                    .catch(function (error) {
+                        res.render('error', {error: error});
+                        console.log(error)
+                    });
+            } else {
+                console.log('js report server is down, using the normal excel generation instead');
+                rp(external)
+                    .then(function (response) {
+                        // console.log(response.content);
+                        for (var i = 0; i < response.content.length; i++) {
+                            if (response.content[i].error.toString().includes("PRODUCT_NOT_FOUND") || response.content[i].error.toString().includes("Product:")) {
+                                result.push(response.content[i]);
+                            }
+                        }
+                        try {
+                            var xls = json2xls(result,{fields:['id','partnerId','error']});
+                            fs.writeFileSync('missing product setup in Atlas.xlsx', xls, 'binary');
+                        }catch (error){
+                            console.log(error);
+                            res.render('error');
+                        }
+                        sendEmail(startDate, endDate, email);
+                        res.render('finishPage', {status: ""});
+
+                        console.log("finish write in file!!");
+
+                    })
+                    .catch(function (error) {
+                        res.render('error', {error: error});
+                        console.log(error)
+                    });
             }
-            // console.log(result);
-            var finalResult = {"content" : result};
-            // console.log(finalResult);
-
-            var data = {
-                template: {
-                    'shortid': 'BJ8POGsL7'
-                },
-                data: finalResult
-            };
-
-            var options = {
-                url: 'https://limitless-reef-70205.herokuapp.com/api/report',
-                method: 'POST',
-                json: data
-            };
-
-            request(options).on('error',function (error) {
-                res.render('error', {error: error});
-            })
-                .pipe(fs.createWriteStream('missing product setup in Atlas.xlsx')).on('finish', function () {
-                sendEmail(startDate, endDate,email);
-                res.render('finishPage',{status:""});
-            })
-                .on('error', function (err) {
-                    res.render('error', {error: err});
-                    console.log(err.message);
-                });
-
-
-            console.log("finish write in file!!");
-
-        })
-        .catch(function (error) {
-            res.render('error',{error: error});
-            console.log(error)
         });
-
+    testAPI.on('error', (e) => {
+        console.error('problem with request: ' + e.message);
+    });
+    testAPI.end();
 });
 
 
-function sendEmail(start, end,email) {
+function sendEmail(start, end, email) {
     var data = fs.readFileSync('./missing product setup in Atlas.xlsx');
 
     const msg = {
