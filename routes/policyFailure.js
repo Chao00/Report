ad591fa31a392f7a75165ea0176c3bbc18b90aa1
Chao <nickzhang0608@gmail.com>
2018-08-23 16:02:34 -0700

    add an alternative solution for report generation

--- routes/policyFailure.js
+++ routes/policyFailure.js
@@ -5,23 +5,25 @@ var rp = require('request-promise');
 const sgMail = require('@sendgrid/mail');
 var fs = require('fs');
 var URL = require('url').URL;
-
+var json2xls = require('json2xls');
 var config = JSON.parse(fs.readFileSync("config.json"));
 var POLICY_FAILURE_URL = JSON.parse(fs.readFileSync("URLS.json"));
+var JS_REPORT_TEST = POLICY_FAILURE_URL.JS_REPORT_TEST;
+var JS_REPORT = POLICY_FAILURE_URL.JS_REPORT;
 
 sgMail.setApiKey(config.API_KEY);
-var side;
+var side,
+    jsReport;
 
 /* GET users listing. */
 router.get('/', function (req, res) {
 
-    res.render('firstPage',{title: "Policy transfer report"});
-    // res.render('finishPage')
+    res.render('firstPage', {title: "Policy transfer report"});
 
 });
 
 
-router.post('/',function (req, res) {
+router.post('/', function (req, res) {
     console.log(req.body.Email);
     console.log(req.body.from);
     console.log(req.body.to);
@@ -33,7 +35,7 @@ router.post('/',function (req, res) {
     var option = req.body.options;
     side = option;
 
-    if (option==='Send both if not specified'){
+    if (option === 'Send both if not specified') {
         option = ''
     }
     side = sideDetection(side);
@@ -43,7 +45,7 @@ router.post('/',function (req, res) {
     // console.log(myUrl.href);
     myUrl.searchParams.set('fromTime', startDate);
     myUrl.searchParams.set('toTime', endDate);
-    myUrl.searchParams.set('monitorType',option);
+    myUrl.searchParams.set('monitorType', option);
     console.log(myUrl.href);
 
 
@@ -54,45 +56,89 @@ router.post('/',function (req, res) {
     };
     // console.log(external.url);
 
-    rp(external)
-        .then(function (response) {
+    var http = require('http'),
+        url = require('url'),
+        options = {
+            method: 'HEAD',
+            host: url.parse(JS_REPORT_TEST).host,
+            port: 80,
+            path: url.parse(JS_REPORT_TEST).pathname
+        },
+        testAPI = http.request(options, function (r) {
+            jsReport = JSON.stringify(r.statusCode);
+            console.log(jsReport);
+            if (jsReport === '200') {
+                rp(external)
+                    .then(function (response) {
 
-            var data = {
-                template: {
-                    'shortid': 'Bk3UUzsUX'
-                },
-                data: response
-            };
+                        var data = {
+                            template: {
+                                'shortid': 'Bk3UUzsUX'
+                            },
+                            data: response
+                        };
 
-            var options = {
-                url: 'https://limitless-reef-70205.herokuapp.com/api/report',
-                method: 'POST',
-                json: data
-            };
+                        var options = {
+                            url: JS_REPORT,
+                            method: 'POST',
+                            json: data
+                        };
 
-            request(options).on('error',function (error) {
-                res.render('error', {error: error});
-            })
-                .pipe(fs.createWriteStream('policy failure.xlsx')).on('finish', function () {
-                sendEmail(startDate, endDate, email);
-                res.render('finishPage',{status:""});
-            })
-                .on('error', function (err) {
-                    res.render('error', {error: err});
-                    console.log(err.message);
-                });
+                        request(options).on('error', function (error) {
+                            res.render('error', {error: error});
+                        })
+                            .pipe(fs.createWriteStream('policy failure.xlsx')).on('finish', function () {
+                            sendEmail(startDate, endDate, email);
+                            res.render('finishPage', {status: ""});
+                        })
+                            .on('error', function (err) {
+                                res.render('error', {error: err});
+                                console.log(err.message);
+                            });
 
 
-            console.log("finish write in file!!");
+                        console.log("finish write in file!!");
 
-        })
-        .catch(function (error) {
-            res.render('error',{error: error});
-            console.log(error)
-        })
+                    })
+                    .catch(function (error) {
+                        res.render('error', {error: error});
+                        console.log(error)
+                    })
+
+            } else {
+                rp(external)
+                    .then(function (response) {
+                        // console.log(response.content);
+                        console.log("finish calling external!!!");
+                        try {
+                            var xls = json2xls(response.content);
+                            fs.writeFileSync('policy failure.xlsx', xls, 'binary');
+                        } catch (error) {
+                            console.log(error);
+                            res.render('error');
+                        }
+
+                        sendEmail(startDate, endDate, email);
+                        res.render('finishPage', {status: ""});
+
+                        console.log("finish write in file!!");
+
+                    })
+                    .catch(function (error) {
+                        res.render('error');
+                        console.log(error)
+                    })
+            }
+        });
+
+    testAPI.on('error', (e) => {
+        console.error('problem with request: ' + e.message);
+    });
+    testAPI.end();
+
 });
 
-  function sendEmail(start, end,email) {
+function sendEmail(start, end, email) {
     var data = fs.readFileSync('./policy failure.xlsx');
 
     const msg = {
@@ -109,10 +155,10 @@ router.post('/',function (req, res) {
             }
         ]
     };
-    sgMail.send(msg, function (err,res) {
+    sgMail.send(msg, function (err, res) {
         if (err) {
             console.log(err);
-            res.render('error',{error:err})
+            res.render('error')
         }
         console.log("Email sent")
 
